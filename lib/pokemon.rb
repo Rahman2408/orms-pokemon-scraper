831c7215b910ec02fed5cac18e64113c25e44068
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-04-30 05:41:09 -0400

    commit tree ae73aa09bd11652eedcd6f8e13161f843e47ec73

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name 
+        @type = type
+        @db = db
+    end 
+
+    def self.save(name, type, db)
+    
+                
+            sql = <<-SQL 
+                    INSERT INTO pokemon (name, type)
+                    VALUES (?,?)
+                    SQL
+        db.execute(sql, name, type)        
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+                SELECT * FROM pokemon 
+                WHERE id = ?
+                SQL
+        new_poke_obj = db.execute(sql, id)[0]
+    self.new(id: new_poke_obj[0], name: new_poke_obj[1], type: new_poke_obj[2], db: db)
+    end
 end
